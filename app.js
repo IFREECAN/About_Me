f7e0e314beb4049eaeee63cde95e5b9b8ca0b641
IFREECAN <jasper.kinnay@gmmail.com>
2017-12-05 16:47:17 -0800

    work with Javascript, CSS and HTML

---
+++ app.js
@@ -0,0 +1,26 @@
+var likeCoding = prompt('do you like coding?');
+console.log('enjoy Coding', likeCoding); //console.log is a note to the coder
+
+if (likeCoding === 'Yes') {
+  alert('Yep! You rock!');
+}
+
+var Outdoors = prompt('like the Outdoors?');
+console.log('like the outdoors', Outdoors);
+
+if (Outdoors === "Yes") {
+  alert('Welcome to the Great-NW');
+}
+
+var food = prompt('do you like sea-food?');
+console.log('like seaa-food', food);
+
+if (food === 'Yes') {
+  alert('Sweettt', we should cook sometimes)
+}
+
+var Art = prompt('do you enjoy art?');
+console.log('like Art', Art);
+if (Art === "Yes") {
+  alert('Nice', Seattle is all about Sights, Sounds and Feelings);
+}
